87ab757e2ea96ece6731e4652382393eda510617
nehalDIU <113442689+sheikhshariarnehal@users.noreply.github.com>
2025-11-27 13:42:10 -0600

    Update project meta tags and improve accessibility
    Revised meta description and keywords in index.html for better SEO and relevance. Enhanced project image alt text in script.js for improved accessibility. Added new CSS styles for project description text in style.css.

--- projects/script.js
+++ projects/script.js
@@ -47,7 +47,7 @@ function showProjects(projects) {
         projectsHTML += `
         <div class="grid-item ${project.category}">
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="${project.name} - ${project.category} Project by Sheikh Shariar Nehal" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
